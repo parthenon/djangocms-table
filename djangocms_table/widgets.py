774186fc236f70f4778fb63dac9db4f86d8af363
Chris Wilson <chris@parthenonsoftware.com>
2021-09-10 16:57:27 -0700

    Updating Django references to support Django 3 Updating Handsontable to remove broken jQuery dependency

--- djangocms_table/widgets.py
+++ djangocms_table/widgets.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.forms import Textarea
+from django.forms.widgets import Textarea
 from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe
 from django.utils.translation.trans_real import get_language
@@ -9,22 +9,17 @@ from djangocms_table.utils import static_url
 class TableWidget(Textarea):
     class Media:
         js = [static_url(path) for path in (
-                'js/jquery-1.8.3.min.js',
-                'js/jquery.handsontable.js',
-                'js/jquery.contextMenu.js',
-                'js/jquery.ui.position.js',
+                'js/handsontable.full.min.js',
                 'js/json2.js',
-                'js/bootstrap-typeahead.js',
             )]
         css = {
             'all': [static_url(path) for path in (
-                'css/jquery.handsontable.css',
-                'css/jquery.contextMenu.css',
+                'css/handsontable.full.min.css',
             )],
         }
 
-    def render_textarea(self, name, value, attrs=None):
-        return super(TableWidget, self).render(name, value, attrs)
+    def render_textarea(self, name, value, attrs=None, renderer=None):
+        return super(TableWidget, self).render(name, value, attrs, renderer)
 
     def render_additions(self, name, value, attrs=None):
         language = get_language().split('-')[0]
@@ -37,6 +32,6 @@ class TableWidget(Textarea):
         return mark_safe(render_to_string(
             'cms/plugins/widgets/table.html', context))
 
-    def render(self, name, value, attrs=None):
-        return self.render_textarea(name, value, attrs) + \
+    def render(self, name, value, attrs=None, renderer=None):
+        return self.render_textarea(name, value, attrs, renderer) + \
             self.render_additions(name, value, attrs)
